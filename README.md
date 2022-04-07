c0d3adf47682411ddeb57c44c76a0eb17075caf3
isabel <isargp@gmail.com>
2022-04-07 11:36:30 +0100

    readme updated. Trying to get useEffect working in FeaturedAlbum

--- README.md
+++ README.md
@@ -2,12 +2,24 @@
 
 Click [here](https://michael-jackson.netlify.app/) to see the webpage.
 
-### React
+## React
 
 This is the first time using React.
 We've been asked to create an app where to see name, biography, type of music of a singer as well as albums and songs.
 I decided to focus this exercise on Michael Jackson which music I will always love.
 
+
+## Install & Use
+
+### Install
+
+- Clone this repo
+- `cd` into the folder `music-app`
+- Run `npm install`
+
+### Use
+
+- Run `npm run dev`
 
 
 ## Plan
--- src/components/Readmore/index.js
+++ src/components/Readmore/index.js
@@ -20,11 +20,9 @@ const ReadMore = ({ children }) => {
 export const Content = () => {
   return (
     <div className="container">
-      <p>
-        <ReadMore>
-          Michael Joseph Jackson (August 29, 1958 – June 25, 2009) was an American singer, songwriter, and dancer. Dubbed the "King of Pop", he is regarded as one of the most significant cultural figures of the 20th century. Over a four-decade career, his contributions to music, dance, and fashion, along with his publicized personal life, made him a global figure in popular culture. Jackson influenced artists across many music genres; through stage and video performances, he popularized complicated dance moves such as the moonwalk, to which he gave the name, as well as the robot. He is the most awarded individual music artist in history.
-        </ReadMore>
-      </p>
+      <ReadMore>
+        Michael Joseph Jackson (August 29, 1958 – June 25, 2009) was an American singer, songwriter, and dancer. Dubbed the "King of Pop", he is regarded as one of the most significant cultural figures of the 20th century. Over a four-decade career, his contributions to music, dance, and fashion, along with his publicized personal life, made him a global figure in popular culture. Jackson influenced artists across many music genres; through stage and video performances, he popularized complicated dance moves such as the moonwalk, to which he gave the name, as well as the robot. He is the most awarded individual music artist in history.
+      </ReadMore>
     </div>
   );
 };
