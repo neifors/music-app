3611f48e1c1402cd4103bb67dea2f59e116498f5
isabel <isargp@gmail.com>
2022-04-08 14:11:23 +0100

    titles+imgs wrapped

--- src/pages/AlbumsGallery/index.js
+++ src/pages/AlbumsGallery/index.js
@@ -9,11 +9,9 @@ export function AlbumsGallery() {
    return(
       <div id="albums-wrapper">
          <div id="albums-title-wrapper">
-            <h1 id="albums-title">
-               <img className="title-imgs" src='https://www.mpadeco.com/resize/500x500/zc/2/f/0/src/sites/mpadeco/files/products/6270.png'/>
-                  ALBUMS DISCOGRAPHY
-               <img className="title-imgs" src='https://pngimg.com/uploads/michael_jackson/michael_jackson_PNG40.png'/>
-            </h1>
+            <img className="title-imgs" src='https://www.mpadeco.com/resize/500x500/zc/2/f/0/src/sites/mpadeco/files/products/6270.png'/>
+            <h1 id="albums-title">   ALBUMS DISCOGRAPHY</h1>
+            <img className="title-imgs" src='https://pngimg.com/uploads/michael_jackson/michael_jackson_PNG40.png'/>
          </div>
          <DisplayGallery />
       </div>
--- src/pages/Biography/index.js
+++ src/pages/Biography/index.js
@@ -10,11 +10,11 @@ export function Biography() {
 
    return(
       <div id="biography">
-         <h1 id="michael-jackson-title">
+         <div id='biography-title-wrapper'>
             <img className="title-imgs" src='https://www.mpadeco.com/resize/500x500/zc/2/f/0/src/sites/mpadeco/files/products/6270.png'/>
-               MICHAEL JACKSON
+            <h1 id="michael-jackson-title">MICHAEL JACKSON</h1>
             <img className="title-imgs" src='https://pngimg.com/uploads/michael_jackson/michael_jackson_PNG40.png'/>
-         </h1>
+         </div>
          <Slideshow interval={10000} images={images} />
          <h2 id="biography-title">Biography</h2>
          <Content />
